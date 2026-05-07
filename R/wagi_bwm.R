5844554f5ce58341f48c983ad16b1e80042a0b6a
MarekDejaUJ <117355919+MarekDejaUJ@users.noreply.github.com>
2026-05-07 14:24:12 +0200

    Dodaj symbol zastępczy dla funkcji „calculate_bwm_weights”
    Added placeholder function for BWM weight calculation.

--- R/wagi_bwm.R
+++ R/wagi_bwm.R
@@ -264,3 +264,10 @@ oblicz_wagi_bwm <- function(nazwy_kryteriow, najlepsze_do_innych, inne_do_najgor
   )
 
 }
+
+#' Calculate BWM weights (placeholder)
+#'
+#' @keywords internal
+calculate_bwm_weights <- function(bwm_criteria, bwm_best, bwm_worst) {
+  stop("calculate_bwm_weights() is not implemented yet. Provide 'weights' instead.")
+}
